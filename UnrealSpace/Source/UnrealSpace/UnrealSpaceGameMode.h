bac06457b3290e3992e65e964883861ada5e06d6
Pawel Miniszewski <pawel.miniszewski@unit9.com>
2015-07-08 13:27:33 +0200

    - created project

---
+++ UnrealSpace/Source/UnrealSpace/UnrealSpaceGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "UnrealSpaceGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALSPACE_API AUnrealSpaceGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
